5c2cd98b73ae0e52d3990b44218642033ada4c7c
Sarim Khan <sarimkhanwwe99@gmail.com>
2021-04-27 22:14:07 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function hideIconBar()
+{
+    var iconBar=document.getElementById("iconBar");
+    var navigation=document.getElementById("navigation");
+    iconBar.setAttribute("style","display:none;");
+    navigation.classList.remove("hide");
+}
+function showIconBar()
+{
+    var iconBar=document.getElementById("iconBar");
+    var navigation=document.getElementById("navigation");
+    iconBar.setAttribute("style","display:block;");
+    navigation.classList.add("hide");
+}
+function showComment()
+{
+    var commentArea=document.getElementById("comment-area");
+    commentArea.setAttribute("style","display:block;");
+    
+}
