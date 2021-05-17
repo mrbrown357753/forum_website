b4f72f0d6483b188c3c44df6040ed8ea4c782970
Sarim Khan <sarimkhanwwe99@gmail.com>
2021-05-17 20:08:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,5 +16,9 @@ function showComment()
 {
     var commentArea=document.getElementById("comment-area");
     commentArea.setAttribute("style","display:block;");
-    
+}
+function showReply()
+{
+    var replyArea=document.getElementById("reply-area");
+    replyArea.setAttribute("style","display:block;");
 }
